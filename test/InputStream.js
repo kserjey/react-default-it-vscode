1b600ca5b3d039af3ee537ff699b764f090cca09
Sergey Korchinskiy <k.serjey@gmail.com>
2018-01-24 22:38:24 +0300

    Add some tests

--- test/InputStream.js
+++ test/InputStream.js
@@ -7,9 +7,20 @@ describe('InputStream', () => {
     assert.strictEqual(stream.current, 'a');
   });
 
-  it('should read while test function is passed', () => {
+  it('should read next char and move cursor', () => {
+    const stream = new InputStream('abcd');
+    assert.strictEqual(stream.next(), 'b');
+  });
+
+  it('should read while test function is passing', () => {
     const stream = new InputStream('012345678');
-    const numbers = stream.readWhile(char => /[0-3]/.test(char));
-    assert.strictEqual(numbers, '0123');
+    stream.readWhile(char => /[0-3]/.test(char));
+    assert.strictEqual(stream.current, '4');
+  });
+
+  it('should return read string', () => {
+    const stream = new InputStream('myname2517');
+    const string = stream.readWhile(char => /[a-z]/.test(char));
+    assert.strictEqual(string, 'myname');
   });
 });
